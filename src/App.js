c253be3a22880c985ba43e2df3094c3d8454c807
sravanthi <sravanthi@insuranceinbox.com>
2019-11-15 01:00:32 +0530

    chart ui and spread sheet done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './App.css';
 import $ from 'jquery';
 import Grid from '@material-ui/core/Grid';
 import MaterialTable from 'material-table';
 import Button from '@material-ui/core/Button';
-import './App.css';
 import Chart from 'chart.js';
 
 let getData = localStorage.getItem('data');
@@ -20,8 +20,8 @@ class App extends React.Component {
                 { title: 'ID', field: 'ID', type: 'numeric'  },
                 { title: 'WorkItem', field: 'WorkItem' , type:"text"},
                 { title: 'Due Date', field: 'DueDate',type:"date"},
-                { title: 'No:of Resources Needed', field: 'resourcesNeeded', type: 'numeric' },
-                { title: 'Work Item Status', field: 'status', lookup: { 4: 'OverDue', 5: 'Done',6:'InProgress'},
+                { title: 'Resources Needed', field: 'resourcesNeeded', type: 'numeric' },
+                { title: 'Work Item Status', field: 'status', lookup: { OverDue: 'OverDue', Done: 'Done',InProgress:'InProgress'},
                 }
             ],
             data:finalData.length>0 ? finalData : []
@@ -33,81 +33,98 @@ class App extends React.Component {
     }
 
     drowChart() {
-        console.log(this.state.data , 'data');
-        var ctx = document.getElementById('chart1').getContext('2d');
-        Chart.defaults.global.tooltips.enabled = true;
-        window.chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'pie',
-            responsive: true,
-            showTooltips: true,
-            data :{
-                datasets: [{
-                    data: [10, 20, 30],
-                    backgroundColor: [
-                        "#FF6384",
-                        "#8463FF",
-                        "#6384FF"
-                    ],
-                }],
-                // These labels appear in the legend and in the tooltips when hovering different arcs
-                labels: [
-                    'overDue',
-                    'Done',
-                    'InProgress'
-                ]
-            }
-        });
-    }
-    upload(e){
-        var form =  { title: 'Due Date', field: 'DueDate',type:"date"},
-            url = 'https://script.google.com/macros/s/AKfycbz92LEtB-nZW7Dw5YQQwMRELq9VpKRCYFkGuZF89qirUiLnzFlw/exec'
-
-        $.fn.serializeObject = function()
-        {
-            var o = {};
-            var a = this.serializeArray();
-            $.each(a, function() {
-                if (o[this.name]) {
-                    if (!o[this.name].push) {
-                        o[this.name] = [o[this.name]];
-                    }
-                    o[this.name].push(this.value || '');
-                } else {
-                    o[this.name] = this.value || '';
+        if(this.state.data.length>0){
+            let OverDue =[];
+            let Done =[];
+            let InProgress =[];
+            this.state.data.filter(a=> a.status === "OverDue" ? OverDue.push(a):'');
+            this.state.data.filter(a=> a.status === "Done" ? Done.push(a):'');
+            this.state.data.filter(a=> a.status === "InProgress" ? InProgress.push(a):'');
+            var ctx = document.getElementById('chart1').getContext('2d');
+            Chart.defaults.global.tooltips.enabled = true;
+            window.chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'pie',
+                responsive: true,
+                showTooltips: true,
+                data :{
+                    datasets: [{
+                        data: [OverDue.length, Done.length, InProgress.length],
+                        backgroundColor: [
+                            "#FF6384",
+                            "#00E1E1",
+                            "#6384FF"
+                        ],
+                    }],
+                    // These labels appear in the legend and in the tooltips when hovering different arcs
+                    labels: [
+                        'overDue',
+                        'Done',
+                        'InProgress'
+                    ]
                 }
             });
-            return o;
-        };
-            e.preventDefault();
-            var jqxhr = $.ajax({
-                url: url,
-                method: "GET",
-                dataType: "json",
-                data: form,
-                success: function(result){
-                    console.log('data', result);
-                }});
-            console.log(jqxhr , '')
+        }
+
+    }
+    upload(e){
+       this.state.data.map(excel => {
+              let excelData= {
+                  ID: excel.ID,
+                  WorkItem: excel.WorkItem,
+                  DueDate: excel.DueDate,
+                  resourcesNeeded: excel.resourcesNeeded,
+                  status: excel.status
+              };
+               let url = 'https://script.google.com/macros/s/AKfycbz92LEtB-nZW7Dw5YQQwMRELq9VpKRCYFkGuZF89qirUiLnzFlw/exec'
+               $.fn.serializeObject = function () {
+                   var o = {};
+                   var a = this.serializeArray();
+                   $.each(a, function () {
+                       if (o[this.name]) {
+                           if (!o[this.name].push) {
+                               o[this.name] = [o[this.name]];
+                           }
+                           o[this.name].push(this.value || '');
+                       } else {
+                           o[this.name] = this.value || '';
+                       }
+                   });
+                   return o;
+               };
+               e.preventDefault();
+               var jqxhr = $.ajax({
+                   url: url,
+                   method: "GET",
+                   dataType: "json",
+                   data:excelData,
+                   success: function (result) {
+                       window.open("https://docs.google.com/spreadsheets/d/1rFdDPwzVQQuO-juMp5SBqxfenN_cFXUt8mkY7KmEt34/edit#gid=0")
+                   }
+               });
+           }
+       )
+
 
     }
     render() {
-        console.log(finalData , 'finalll dataaa');
         return(
             <div className="App">
                 <header className="App-header">
                   <img src="https://www.zen3.com/wp-content/uploads/2019/04/zen3-logo.png" className="App-logo" alt="Zen3" />
                 </header>
-
                 <div className="margin-5">
                     <div style={{maxWidth:'300px', margin:'auto'}} >
-                    <canvas id={"chart1"} width="300" height="190"/>
+                        <h4>Work Status Metrics</h4>
+                        {this.state.data.length>0 ?
+                            <canvas id={"chart1"} width="300" height="190"/>:<p>No Data</p>
+                        }
+
                     </div>
                 </div>
-
                 <div className="mainSub">
                     <div>
-                        <Grid container >
+                        <Grid container>
                             <Grid item xs={4}>
                                 <h4>List Of Work Items</h4>
                             </Grid>
@@ -117,7 +134,7 @@ class App extends React.Component {
                         </Grid>
                     </div>
                     <div className="buttonMain" >
-                        <Button variant="contained" color="primary" onClick={(e)=> this.upload(e)} >
+                        <Button variant="contained" color="primary" onClick={(e)=>{this.state.data.length > 0 ? this.upload(e) : alert('No Data to upload')}} >
                             Upload to google spread sheet
                         </Button>
                     </div>
@@ -137,7 +154,7 @@ class App extends React.Component {
                                             data.push(newData);
                                             localStorage.setItem('data',JSON.stringify(data));
                                             return { ...prevState, data };
-                                        });
+                                        },()=>this.drowChart());
                                     }, 600);
                                 }),
                             onRowUpdate: (newData, oldData) =>
@@ -150,7 +167,7 @@ class App extends React.Component {
                                                 data[data.indexOf(oldData)] = newData;
                                                 localStorage.setItem('data',JSON.stringify(data));
                                                 return { ...prevState, data };
-                                            });
+                                            },()=>this.drowChart());
                                         }
                                     }, 600);
                                 }),
@@ -163,7 +180,7 @@ class App extends React.Component {
                                             data.splice(data.indexOf(oldData), 1);
                                             localStorage.setItem('data',JSON.stringify(data));
                                             return { ...prevState, data };
-                                        });
+                                        },()=>this.drowChart());
                                     }, 600);
                                 }),
                         }}

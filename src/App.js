4f8d7cdd505b93e9fd78962f676b3d6b2dc2518b
sravanthi <sravanthi@insuranceinbox.com>
2019-11-14 17:50:21 +0530

    spredsheet changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import MaterialTable from 'material-table';
 import Button from '@material-ui/core/Button';
 import './App.css';
+import Creds from './client_secret.json';
+import GoogleSpreadsheet from 'google-spreadsheet';
 
 
-var getData = localStorage.getItem('data');
 var finalData =[];
-if(getData !== null && getData.length>0){
-    finalData = JSON.parse(getData);
-}
 
-
-console.log(finalData,'finalData');
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -23,24 +18,50 @@ class App extends React.Component {
                 { title: 'WorkItem', field: 'WorkItem' , type:"text"},
                 { title: 'Due Date', field: 'DueDate',type:"date"},
                 { title: 'No:of Resources Needed', field: 'resourcesNeeded', type: 'numeric' },
-                {
-                    title: 'Work Item Status',
-                    field: 'status',
-                    lookup: { 4: 'OverDue',
-                              5: 'Done' ,
-                              6:'InProgress' },
+                { title: 'Work Item Status', field: 'status', lookup: { 4: 'OverDue', 5: 'Done',6:'InProgress'},
                 }
             ],
             data:finalData.length>0 ? finalData : []
         }
     }
 
-  /*  componentWillMount(){
-        if(this.state.data.length >0){
-            this.setState({data : localStorage.getItem('data')})
+    componentWillMount(){
+        let getData = localStorage.getItem('data');
+        if(getData !== null && getData.length>0){
+            finalData = JSON.parse(getData);
         }
+    }
+    uploadExcel(){
 
-    }*/
+
+        var doc = new GoogleSpreadsheet('1edp7bkIN9Hnlj351ilAYvIahWZ3a2-NrFdebQJJbrsY');
+        console.log(doc , 'docssss');
+        console.log(Creds , 'credscredscreds');
+        // Authenticate with the Google Spreadsheets API.
+        doc.useServiceAccountAuth(Creds, function (err) {
+            if (err) console.log(err);
+            doc.getInfo(function (err, info) {
+                console.log(info , 'infioooo');
+                console.log(err , 'errooo');
+                console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+                let sheet = info.worksheets[0];
+                console.log('sheet #1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+
+                var newrow = {
+                    title: 'banana',
+                    type: 'fruit',
+                    url: 'http://example.com'
+                };
+                doc.addRow(1, newrow, function( err, rows ){
+                    if (err) console.log(err);
+                    console.log(rows);
+
+                });
+
+            })
+        });
+    }
+
     render() {
         console.log(finalData , 'finalll dataaa');
         return(
@@ -59,13 +80,14 @@ class App extends React.Component {
                             </Grid>
                         </Grid>
                     </div>
-                    <div className="buttonMain">
-                        <Button variant="contained" color="primary">
+                    <div className="buttonMain" >
+                        <Button variant="contained" color="primary" onClick={()=> this.uploadExcel()}>
                             Upload to google spread sheet
                         </Button>
                     </div>
                 <div className="table-comp">
                     <MaterialTable
+                        style={{width:'100%'}}
                         title=""
                         columns={this.state.columns}
                         data={this.state.data}
@@ -110,9 +132,9 @@ class App extends React.Component {
                                 }),
                         }}
                     />
-                        </div>
                 </div>
-            </div>
+             </div>
+          </div>
     );}
     }
 
